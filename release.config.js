6a8fda089be08e7c742640051d65df11cbae2528
Kellen Frodelius-Fujimoto <kellen@kellenfujimoto.com>
2026-04-14 09:40:16 -0700

    ci: update semantic-release config to release from all branches

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 /** @type {import('semantic-release').GlobalConfig} */
 export default {
-  branches: ['main'],
+  branches: ['*'],
   dryRun: process.env.DRY_RUN === 'true',
   plugins: [
     '@semantic-release/commit-analyzer',
